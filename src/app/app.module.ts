51d4827975c4c362ad46372c7161797644c94b75
Pavan Pandey <pandeypavan03@gmail.com>
2020-10-30 20:06:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { EditTaskComponent } from './pages/edit-task/edit-task.component';
+import { EditListComponent } from './pages/edit-list/edit-list.component';
+import { NewTaskComponent } from './pages/new-task/new-task.component';
+import { NewListComponent } from './pages/new-list/new-list.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { SignupPageComponent } from './pages/signup-page/signup-page.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskViewComponent,
+    EditTaskComponent,
+    EditListComponent,
+    NewTaskComponent,
+    NewListComponent,
+    LoginPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
